34e31aa03d40b70769d6313d5a905ab1edea89db
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-03 03:06:10 +0530

    feat: desired category navigation from home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
-import { useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 
 import "./HomePage.css";
-import {Footer} from "../../components/Footer/Footer"
+import { Footer } from "../../components/Footer/Footer"
 import { categories } from "../../backend/db/categories";
+import { useContext } from "react";
+import { FilterContext } from "../../contexts/FilterProvider";
+import { ACTIONS } from "../../reducers/FilterReducer";
 
 export function HomePage() {
 
     const navigate = useNavigate();
+
+    const {dispatchFilter} = useContext(FilterContext);
+    const {SET_CATEGORY_FILTER} = ACTIONS
+
+    const categoryNavigationHandler = (categ) => dispatchFilter({type: SET_CATEGORY_FILTER, payload: categ})
 
     return (
         <>
@@ -15,7 +23,7 @@ export function HomePage() {
                 <div className="main-img-container">
                     <img className="main-img" src="assets/images/homepageimg.jpg" alt="" onClick={() => navigate("/productlist")} />
                 </div>
-                
+
                 <div className="category-container">
 
                     <div className="container-heading">
@@ -24,21 +32,23 @@ export function HomePage() {
 
                     <div className="categories-list">
                         {categories.map(({ _id, categoryName, description, image }) => (
-                            <div key={_id} className="category-item">
-                                <div className="category-img-container">
-                                    <img className="category-img" src={image} alt="" />
+                            <NavLink to="/productlist" onClick={() => categoryNavigationHandler(categoryName.toUpperCase())}>
+                                <div key={_id} className="category-item">
+                                    <div className="category-img-container">
+                                        <img className="category-img" src={image} alt="" />
+                                    </div>
+                                    <div className="category-name">
+                                        <h3>{categoryName}</h3>
+                                    </div>
                                 </div>
-                                <div className="category-name">
-                                    <h3>{categoryName}</h3>
-                                </div>
-                            </div>
+                            </NavLink>
                         ))}
                     </div>
 
                 </div>
             </div>
 
-            <Footer/>
+            <Footer />
         </>
     )
 }
